f5d9620fa857441ac0d3d939bb721561ab866ebf
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2023-12-18 23:12:20 +0500

    updated-version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatinetChartComponent } from './components/patinet-chart/patinet-chart.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path : '',
+    component : PatinetChartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatinetChartComponent } from './components/patinet-chart/patinet-chart.component';
+
+import {HttpClientModule} from '@angular/common/http'
+import {HighchartsChartModule} from 'highcharts-angular';
+import {DatePipe} from '@angular/common'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatinetChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HighchartsChartModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
