eb86456d1df765e3ddb06f4f00316ab6d466b469
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2024-10-13 14:31:08 +0500

    UPDATE

--- src/app/components/PDFS/pdf-1/pdf-1.component.ts
+++ src/app/components/PDFS/pdf-1/pdf-1.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import {jsPDF} from 'jspdf'
+import html2canvas from 'html2canvas'
+import { saveAs } from 'file-saver';
 
 interface PatientVariantData {
   ACMGClassification: string;
@@ -36,7 +39,47 @@ export class Pdf1Component implements OnInit {
     });
   }
 
-
+  convertHtmlToPdf(){
+    const element = document.getElementById('pdf-1') as HTMLElement; // Replace with 'pdf-1' or your element ID
+    if (element) {
+      html2canvas(element, { allowTaint: true, useCORS: true }).then((canvas: any) => {
+        // Create the PDF with dynamic size based on the canvas dimensions
+        const doc = new jsPDF({
+          orientation: 'portrait',
+          unit: 'px',
+          format: [0.562 * canvas.width, 0.562 * canvas.height] // Scale based on canvas size
+        });
+  
+        // Add the canvas content to the PDF
+        doc.addImage(canvas.toDataURL("image/jpeg"), "JPEG", 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height);
+  
+        // Create FormData to send the file
+        const formData = new FormData();
+  
+        setTimeout(() => {
+          // Convert the PDF to a blob
+          const blob = doc.output('blob');
+          const file = new File([blob], 'pdf-1.pdf'); // Name the PDF file
+  
+          // Append the file to FormData
+          formData.append('file', file);
+  
+          // Log the blob and trigger file download (if needed)
+          console.log(blob);
+          saveAs(blob, 'pdf-1.pdf');
+           //pdf.save('reqform.pdf');
+  
+        }, 1000); // Timeout to ensure the PDF is properly generated
+      }).catch(error => {
+        console.error("Error generating PDF:", error);
+      });
+    } else {
+      console.error("Element with id 'pdf-1' not found.");
+    }
+  }
+  
+  
+  
 
   loadData(){
     if (this.accessionId) { // Ensure analysisId is not null or undefined
